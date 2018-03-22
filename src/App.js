999419a73762c7c157fcb264a3f45dee3aeb64f7
tanguy chennaux <tanguy.chennaux@gmail.com>
2018-03-22 19:04:42 +0000

    Person component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <p>I am a Genius!!!</p>
+        <Person />
       </div>
     );
   }
