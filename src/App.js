4857eaaafa691a4457b49e586d0711a04d898305
tanguy chennaux <tanguy.chennaux@gmail.com>
2018-03-30 15:31:35 +0100

    media query with React and Radium

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium, { StyleRoot} from 'radium';
 import Person from './Person/Person';
 
 
@@ -49,12 +50,17 @@ class App extends Component {
   render () {
 
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
-    }
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
+    };
 
     let persons = null;
 
@@ -71,19 +77,27 @@ class App extends Component {
           })}
         </div>
       );
+
+      style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
     }
 
 
     return (
-      <div className="App">
-        <p>I am a Genius!!!</p>
-        <button
-        style={style}
-        onClick={this.togglePersonHandler}>Toggle Persons</button>
-        {persons}
-      </div>
+      <StyleRoot>
+        <div className="App">
+          <p>I am a Genius!!!</p>
+          <button
+          style={style}
+          onClick={this.togglePersonHandler}>Toggle Persons</button>
+          {persons}
+        </div>
+      </StyleRoot>
     );
   }
 }
 
-export default App;
+export default Radium(App);
