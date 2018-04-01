a27caed813a12e989598c216da011a29fdea9de1
tanguy chennaux <tanguy.chennaux@gmail.com>
2018-04-01 12:05:38 +0100

    added PureComponents which checks for updates in react only renders changes

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,8 +1,8 @@
-import React, {Component} from 'react';
+import React, {PureComponent} from 'react';
 import Person from './Person/Person';
 
 
-class Persons extends Component {
+class Persons extends PureComponent {
 
   constructor(props) {
     super(props);
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
-class App extends Component {
+class App extends PureComponent {
   constructor(props) {
     super(props);
     console.log('[App.js] Inside Constructor', props);
