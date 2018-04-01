5663dd16e1c4e4d83d39d398aaa6041473f8a952
tanguy chennaux <tanguy.chennaux@gmail.com>
2018-04-01 11:29:07 +0100

    components lifecycle with console log at each steps to show the flow of the app

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,20 @@ import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log('[App.js] Inside Constructor', props);
+
+  }
+
+  componentWillMount () {
+    console.log ('[App.js] Inside componentWillMount ()');
+  }
+
+  componentDidMount () {
+    console.log ('[App.js] Inside componentDidMount ()');
+  }
+
 
   state =  {
     persons: [
@@ -47,7 +61,7 @@ class App extends Component {
   }
 
   render () {
-
+    console.log('[App.js] Inside render ()');
     let persons = null;
 
     if (this.state.showPersons) {
